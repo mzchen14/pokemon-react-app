0ad53cb308e5d4d54a856655bfc6b551a0bce236
Ming Chen <mzchen14@gmail.com>
2020-06-02 16:14:13 -0400

    Updated CSS and basic pokedex component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,20 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, {Component} from 'react'
 import './Pokecard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+
+//function to pad numbers to use higher quality images from our img api
+let padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3) : num)
+
 class Pokecard extends Component {
     render() {
-        let imgSrc = `${POKE_API}${this.props.id}.png`
+        let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
         return(
             <div className="Pokecard"> 
-                <h1>{this.props.name}</h1>
+                <h1 className="Pokecard-title">{this.props.name}</h1>
                 <img src={imgSrc} alt={this.props.name}/>
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.exp}</div>
+                <div className="Pokecard-data">Type: {this.props.type}</div>
+                <div className="Pokecard-data">EXP: {this.props.exp}</div>
             </div>
         )
     }
